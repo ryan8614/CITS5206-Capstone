c749392f92955ceaf66fb847157260b6b79b60c9
Ryaaan_ <156677666+ryan8614@users.noreply.github.com>
2025-03-18 15:33:45 +0800

    Solve npm run build faild.

--- excel-sync/.next/server/server-reference-manifest.js
+++ excel-sync/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lJUkSOpdYNg9IB7gapszbN1AZ2enQuKSGIT78USSWDs=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- excel-sync/src/app/layout.tsx
+++ excel-sync/src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { ADLaM_Display, Geist, Geist_Mono } from "next/font/google";
+import { ADLaM_Display } from "next/font/google";
 import "./globals.css";
 
 // font style
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-}); 
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const adlam_display = ADLaM_Display({
   subsets: ["latin"], 
   weight: "400"
